f6c2c1c2ed19f7b621166c0df7cffe35de01cd77
Liam <lccoombs@hotmail.co.uk>
2018-10-09 08:42:37 +0000

    balance method and initialize created

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,8 @@
 class Oystercard
-    
+  def initialize
+    @balance = 0.00
+  end
+  def balance
+    @balance
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
-    
+  describe '#balance' do
+    it 'responds to balance' do
+      expect(subject).to respond_to(:balance)
+    end
+    it 'balance should be 0 on initialization' do
+      expect(subject.balance).to eq 0.00
+    end
+  end
 end
